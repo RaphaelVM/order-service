7d71564f2c6fd20a85348b8154ecdbf7b8f71dbe
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-06-06 20:17:41 +0200

    Update RabbitMQConsumer.java

--- src/main/java/com/drossdrop/orderservice/rabbitmq/RabbitMQConsumer.java
+++ src/main/java/com/drossdrop/orderservice/rabbitmq/RabbitMQConsumer.java
@@ -2,6 +2,8 @@ package com.drossdrop.orderservice.rabbitmq;
 
 import com.drossdrop.orderservice.model.Product;
 import com.drossdrop.orderservice.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,14 @@ public class RabbitMQConsumer {
     }
 
     @RabbitListener(queues = "create-product-command")
-    public void receiveCreateProductCommand(Product product) {
+    public void receiveCreateProductCommand(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product product = null;
+        try {
+            product = objectMapper.readValue(json, Product.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         productService.createProduct(product);
         log.info("Product is {} created", product);
     }
